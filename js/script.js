1e16a20233e891f1119c4123e1be85c583bcafdb
Darko <darko1290@gmail.com>
2018-08-16 19:30:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 	$J = jQuery;
-	$( function () {
-        $( '#BGItems figure img' ).each( function () {
-            var strItemID = $( this ).attr( 'data-id' );
-            if (strItemID !='customitem'){
-            $( this ).attr( 'src', 'http://cdn.dota2.com/apps/dota2/images/items/' + strItemID + '_lg.png' );
-            }
-        });
-        $( '#BGHeroes figure img' ).each( function () {
-            var strHeroID = $( this ).attr( 'data-id' );
-            if (strHeroID !='customhero'){
-            $( this ).attr( 'src', 'http://cdn.dota2.com/apps/dota2/images/heroes/' + strHeroID + '_full.png' );
-            }
-        });
-    });
+$( function () {
+        $( '#BGItems figure img' ).each( 
+		function () {
+			var strItemID = $( this ).attr( 'data-id' );
+			if (strItemID !='customitem'){
+				$( this ).attr( 'src', 'http://cdn.dota2.com/apps/dota2/images/items/' + strItemID + '_lg.png' );
+			}
+		}
+	);
+        $( '#BGHeroes figure img' ).each( 
+		function () {
+			var strHeroID = $( this ).attr( 'data-id' );
+			if (strHeroID !='customhero'){
+				$( this ).attr( 'src', 'http://cdn.dota2.com/apps/dota2/images/heroes/' + strHeroID + '_full.png' );
+			}
+		}
+	);
+});
